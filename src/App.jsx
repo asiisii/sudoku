be15d8bb2303c004e17bbe1cf98e501fa830f9ed
Ashish Malla <ashishmalla45@gmail.com>
2022-10-12 14:56:46 -0600

    Add validation check for wrong guess and styled the cell based on it

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { easyMode, easyAnswer } from './data'
+import { easyMode, easyModeAnswer } from './data'
 import './App.css'
 
-// export const easy_boards = [
-// 	[2, -1, 5, -1, -1, -1, 7, -1, -1],
-// 	[4, 5, -1, -1, -1, 9, -1, -1, -1],
-// 	[-1, 2, -1, 6, -1, 8, 1, -1, -1],
-// 	[-1, -1, 9, -1, -1, -1, 8, 5, 6],
-// 	[7, -1, -1, -1, -1, -1, -1, -1, 2],
-// 	[4, 1, 8, -1, -1, -1, 2, -1, -1],
-// 	[-1, -1, 4, 3, -1, 7, -1, 1, -1],
-// 	[-1, -1, -1, 1, -1, -1, -1, 8, 5],
-// 	[-1, -1, 6, -1, -1, -1, 7, -1, 8],
-// ]
-
-const createDefaultValue = gameMode => {
+const boardSetUp = gameMode => {
 	const defaultValues = [1, 2, 3, 4, 5, 6, 7, 8, 9].reduce(
 		(arr, currEl, currIndex) => {
 			const a = arr.push(
@@ -29,8 +17,13 @@ const createDefaultValue = gameMode => {
 
 const App = () => {
 	const [board, setBoard] = useState([])
+	const [solution, setSolution] = useState([])
+	const [mistakes, setMistakes] = useState(0)
 
-	useEffect(() => setBoard(createDefaultValue(easyMode)), [])
+	useEffect(() => {
+		setBoard(boardSetUp(easyMode))
+		setSolution(boardSetUp(easyModeAnswer))
+	}, [])
 
 	const createBoard = () => {
 		return (
@@ -43,12 +36,16 @@ const App = () => {
 									return (
 										<td key={rIndex + cIndex}>
 											<input
-												className='cell'
+												className={`cell 
+                        ${(row === 2 || row === 5) && 'upper-horizontal-line'} 
+                        ${(row === 3 || row === 6) && 'lower-horizontal-line'}
+                        ${(col === 2 || col === 5) && 'left-vertical-line'} 
+                        ${(col === 3 || col === 6) && 'right-vertical-line'}
+                        ${boardSetUp(easyMode)[row][col] !== '-' && 'disabled'}
+                        `}
 												onChange={e => handleChange(e, row, col)}
 												value={board[row][col] === '-' ? '' : board[row][col]}
-												disabled={
-													createDefaultValue(easyMode)[row][col] !== '-'
-												}
+												disabled={boardSetUp(easyMode)[row][col] !== '-'}
 											/>
 										</td>
 									)
@@ -61,20 +58,36 @@ const App = () => {
 		)
 	}
 
+	const isGameOver = () => document.querySelectorAll('input').forEach(el => (el.disabled = true))
+
 	const handleChange = (e, row, col) => {
 		const val = e.target.value
 		let newBoard = JSON.parse(JSON.stringify(board))
 		if (val === '' || (val >= 1 && val <= 9)) {
 			newBoard[row][col] = val
+			if (
+				newBoard[row][col] !== '' &&
+				newBoard[row][col] !== solution[row][col]
+			) {
+				e.target.classList.add('incorrect')
+				setMistakes(mistakes + 1)
+				if (mistakes === 2) isGameOver()
+			} else {
+				e.target.classList.remove('incorrect')
+			}
 		}
+
 		setBoard(newBoard)
 	}
 
 	return (
 		<div className='container'>
+			<h1 className='game-title'>Sudoku</h1>
 			<div className='board-wrapper'>
-				<h1 className='game-title'>Sudoku</h1>
-				{board.length > 0 && createBoard()}
+				<div className='board'>{board.length > 0 && createBoard()}</div>
+				<div className='game-info'>
+					<h3 className='error-count'>Mistakes: {mistakes}/3</h3>
+				</div>
 			</div>
 		</div>
 	)
